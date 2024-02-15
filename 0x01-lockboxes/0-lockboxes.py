ed1220cb7c7ee930f05e80a52fc8d318a81a7ed1
Jess <jessgatura@gmail.com>
2024-02-16 02:01:16 +0300

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""module containing lockboxes function"""
+
+
+def canUnlockAll(boxes):
+    """ lockboxes function"""
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+    queue = [0]
+
+    while queue:
+        current_box = queue.pop(0)
+        for key in boxes[current_box]:
+            if 0 <= key < n and not visited[key]:
+                visited[key] = True
+                queue.append(key)
+
+    return all(visited)
