c32126428871a2808e7d27b5367aafcb480ec3a0
Jess <jessgatura@gmail.com>
2024-03-01 02:09:40 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,50 +2,36 @@
 """log parsing script"""
 
 import sys
-from collections import defaultdict
 
 
-def parse_line(line):
-    """parsing a line"""
-    parts = line.split()
-    if len(parts) != 10:
-        return None
-    ip_address, _, _, status_code, file_size = parts[:5]
-    if not status_code.isdigit():
-        return None
-    return ip_address, int(status_code), int(file_size)
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+counter = 0
 
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
 
-def print_metrics(total_file_size, status_code_counts):
-    """prints the metrics"""
-    print("Total file size: {}".format(total_file_size))
-    for status_code in sorted(status_code_counts.keys()):
-        print("{}: {}".format(status_code, status_code_counts[status_code]))
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
+except Exception as err:
+    pass
 
-def main():
-    """main function"""
-    total_file_size = 0
-    status_code_counts = defaultdict(int)
-    line_count = 0
-
-    try:
-        for line in sys.stdin:
-            line = line.strip()
-            parsed_line = parse_line(line)
-            if parsed_line:
-                ip_address, status_code, file_size = parsed_line
-                total_file_size += file_size
-                status_code_counts[status_code] += 1
-                line_count += 1
-
-            if line_count == 10:
-                print_metrics(total_file_size, status_code_counts)
-                line_count = 0
-
-    except KeyboardInterrupt:
-        print_metrics(total_file_size, status_code_counts)
-
-
-if __name__ == "__main__":
-    main()
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
